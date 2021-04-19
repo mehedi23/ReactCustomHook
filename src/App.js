507da961d01fcedcb3de392eb205937af0ab602d
mehedi23 <satusatusatu0@gmail.com>
2021-04-19 15:26:03 +0600

    each other val

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import React , {useState} from "react"
 import './App.css';
 
 function App() {
+  const [ first , setFirst ] = useState('');
+  const [ second , setSecond ] = useState('');
+  const [ third , setThird ] = useState('');
+
+  const first_name = (e) => {
+    setFirst(e.target.value)
+  }
+
+  const second_name = (e) => {
+    setSecond(e.target.value)
+  }
+
+  const third_name = (e) => {
+    setThird(e.target.value)
+  }
+
+  const outPut = e => {
+    const form_val = [ first , second , third ];
+    console.log(form_val)
+    setFirst('')
+    setSecond('')
+    setThird('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <h1 style={{
+              "marginBottom": "50px"
+          }}>
+              Form validation
+          </h1>
+
+          <input 
+            value ={first}
+            onChange={first_name}
+            name="first name" 
+            placeholder="Your First Name"
+          />
+
+          <input 
+            value={second}
+            onChange={second_name}
+            name="second name" 
+            placeholder="Your Second Name"
+          />
+
+          <input 
+            value={third}
+            onChange={third_name}
+            name="third name" 
+            placeholder="Your Third Name"
+          />
+
+          <button onClick={outPut}> Submit </button>
+
+      </div>
   );
 }
 
